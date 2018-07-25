975a69e6cee944780eaa477348852c5c4ebe96f4
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-25 20:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
     return 'YES INDEED!';
   }
   else if (string== "I love you, Granda."){
-    return 'I love you, too';
+    return 'I love you, too.';
   }
   else if (string.toLowerCase()==string){
     return 'I can\'t hear you!';
