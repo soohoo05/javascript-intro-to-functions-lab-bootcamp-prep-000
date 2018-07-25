03ad39752984c8e6232a0bed3d454fa05e9a0cb8
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-25 20:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function shout (string){
 
 }
 
-whisper(string){
+function whisper(string){
   return string.toLowerCase();
 }
 
-logShout(string){
+function logShout(string){
   console.log(string.toUpperCase());
 }
 
-logWhisper(string){
+function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-sayHiToGrandma(string){
+function sayHiToGrandma(string){
   if(string.toUpperCase()==string){
     return 'YES INDEED!';
   }
